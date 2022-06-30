850085c6425e69bae1f9af49cb5c5ccf4ca6b54d
Tiffani Burk <greengurl1088@yahoo.com>
2022-06-30 13:27:20 -0500

    custom event listeners added for total price

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,11 +1,44 @@
-import { getOrders } from "./database.js"
+import { getMetals, getOrders, getSizes, getStyles } from "./database.js"
+const metals = getMetals()
+const styles = getStyles()
+const sizes = getSizes()
 
+
+//This function calvculates the total cost of the items matches the id of the nested functions
 const buildOrderListItem = (order) => {
+//finding the price of the chosen metal
+const foundMetal = metals.find(
+    (metal) => {
+        return metal.id === order.metalId
+    }
+)
+//find price of chosen size
+const foundSize = sizes.find(
+    (size) => {
+        return size.id === order.sizeId
+    }
+)
+//find price of chosen style
+const foundStyle = styles.find(
+    (style) => {
+        return style.id === order.styleId
+    }
+)
+//store value of all prices to a variable
+const totalCost = foundMetal.price + foundSize.price + foundStyle.price
+
+//converts a number to a string
+const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+})
+//interpolate the price with an HTML string
     return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
+        Order #${order.id} was placed on ${order.timestamp} and costs ${costString}
     </li>`
 }
 
+//This functon is returning a list of the orders the customer places
 export const Orders = () => {
     /*
         Can you explain why the state variable has to be inside
@@ -22,4 +55,5 @@ export const Orders = () => {
 
     return html
 }
+
 
